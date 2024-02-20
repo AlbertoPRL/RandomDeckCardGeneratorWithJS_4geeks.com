fd56243d8e4fdf70823934fb2a71bfb54d4f8208
AlbertoPRL <67210580+AlbertoPRL@users.noreply.github.com>
2024-02-19 23:44:14 -0500

    AddedResizableCardAndIntervalButton

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,51 @@
 import '../scss/styles.scss'
 import 'bootstrap'
 
-const generateCard = () => {
-    const pips = document.querySelectorAll(".pip");
-    const value = document.querySelector("#value");
+const button = document.querySelector("#button");
+const timeoutButton = document.querySelector("#timeout-button");
+const pips = document.querySelectorAll(".pip");
+const value = document.querySelector("#value");
+const resizableCard = document.querySelector("#resizable-card");
+const widthInput = document.querySelector("#width-input");
+const heightInput = document.querySelector("#height-input");
+const submitCardSizeChangeButton = document.querySelector("#change-card-size");
 
-    const valueArray = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-    const pipArray= [
+let isIntervalSet = false;
+let intervalId = null;
+
+
+const generateCard = () => {
+    const valueArray = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const pipArray = [
         '<i class="bi bi-suit-club-fill pip-size"></i>',
         '<i class="bi bi-suit-spade-fill pip-size"></i>',
         '<i class="bi bi-diamond-fill pip-size" style="color: red"></i>',
         '<i class="bi bi-suit-heart-fill pip-size" style="color: red"></i>'
     ];
     const cardValue = valueArray[randomIndex(valueArray)];
-    
-    if(cardValue === 'A')
-    {
-        const cardPip = pipArray[randomIndex(pipArray)]; 
-        value.innerHTML = cardPip;
-        for(let pip of pips)
-        {
-            pip.innerHTML = cardValue;
-            pip.classList.add("pip");
-        }
-        console.log("1");
+    const cardPip = pipArray[randomIndex(pipArray)];
+    value.innerHTML = cardValue;
+    for (let pip of pips) {
+        pip.innerHTML = cardPip;
+        pip.classList.add("pip");
     }
-    else
-    {
-        const cardPip = pipArray[randomIndex(pipArray)];
-        value.innerHTML = cardValue;
-        for(let pip of pips)
-        {
-            pip.innerHTML = cardPip;
-            pip.classList.add("pip");
-        }
-        console.log("2")
+};
+
+const generateCardWithTimeoutToggle = () => {
+    if (isIntervalSet) {
+        clearInterval(intervalId)
+        timeoutButton.innerHTML = "Set interval 10s";
     }
+    else {
+        intervalId = setInterval(generateCard, 1000);
+        timeoutButton.innerHTML = "Stop interval";
+    }
+    isIntervalSet = !isIntervalSet;
+}
+
+const changeCardSize = () => {
+    resizableCard.style.width = `${Number(widthInput.value)}px`;
+    resizableCard.style.height = `${Number(heightInput.value)}px`;
 }
 
 const randomIndex = (array) => {
@@ -43,5 +53,8 @@ const randomIndex = (array) => {
 };
 
 window.onload = () => {
+    button.addEventListener("click", generateCard, false);
+    timeoutButton.addEventListener("click", generateCardWithTimeoutToggle, false);
+    submitCardSizeChangeButton.addEventListener("click", changeCardSize, false);
     generateCard();
 }
